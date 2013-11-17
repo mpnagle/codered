5350a00a1ed50dce1a36621d2a9ed78024a6bc39
Michael Nagle <mpnagle@gmail.com>
2013-11-16 17:05:16 -0800

    committing alert buttons

--- codered.js
+++ codered.js
@@ -1,3 +1,5 @@
+Alerts = new Meteor.Collection("alerts");
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to codered.";
@@ -10,8 +12,36 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
-}
 
+
+  Template.main.events({
+      'click #red' : function() {
+	  Session.set("alertLevel", 2);
+      },
+      'click #yellow' : function() {
+	  Session.set("alertLevel", 1);
+      },
+      'click #green' : function() {
+	  Session.set("alertLevel", 0);
+      },
+      
+      'click #submit' : function() {
+	  Alerts.insert({
+	      alertLevel: Session.get("alertLevel"), 
+	      message: $("#message").val()
+	  })
+	  console.log($("#message").val());
+      }
+  });
+
+
+
+  Template.main.alertLevel = function(){
+      return Session.get("alertLevel");
+  }
+
+
+}
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup

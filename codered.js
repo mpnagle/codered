188c40376c19cbfed064afe40505ca69d0103bb1
Michael Nagle <mpnagle@gmail.com>
2013-11-17 15:27:15 -0800

    working twilio

--- codered.js
+++ codered.js
@@ -11,36 +11,11 @@ if (Meteor.isClient) {
 	  $('body').css("background-color","#FFE0E0");
 
 
-	  HTTP.call('post',
-		    'https://api.twilio.com/2010-04-01/Accounts/AC504dcad90a9b4647b890e9b1f71025ba/SMS/Messages.json',
-		    {
-			params:{From:'+14014415503', To:'+16175711369', Body: 'ZOMG'},
-			    auth: 'AC5ef8732a3c49700934481addd5ce1659:c2ba241dca61e7c363703f9c393f8099',
-			    headers: {'content-type':'application/x-www-form-urlencoded'}
-		    }, function () {
-			console.log(arguments)
-			    }
-		    );
+	  
 
 
 
 
-	  /*
-twilio = Twilio("AC504dcad90a9b4647b890e9b1f71025ba", "c2ba241dca61e7c363703f9c393f8099");
-	  twilio.sendSms({
-		  to:'+16175711369', // Any number Twilio can deliver to
-		      from: '+14014415503', // Twilio number
-		      body: 'word to your mother.' // body of the SMS message
-		      }, function(err, responseData) { //this function is executed when a response is received from Twilio
-		  if (!err) { // "err" is an error received during the request, if any
-		      // "responseData" is a JavaScript object containing data received from Twilio.
-		      // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-		      // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-		      console.log(responseData.from); // outputs "+14506667788"
-		      console.log(responseData.body); // outputs "word to your mother."
-		  }
-	      });
-	  */
 
       },
       'click #yellow' : function() {
@@ -74,7 +49,7 @@ twilio = Twilio("AC504dcad90a9b4647b890e9b1f71025ba", "c2ba241dca61e7c363703f9c3
 	  
 		      
 	  
-		  
+	  	  Meteor.call("postTwilio");
 	  
 	  //	  console.log(Alerts.find(
 	  //		  {sort: {$natural: -1}}
@@ -117,27 +92,6 @@ twilio = Twilio("AC504dcad90a9b4647b890e9b1f71025ba", "c2ba241dca61e7c363703f9c3
   
   Template.friends.events({
       'click #submit': function(){
-	  /* 
-	  emailAddress = $("#submit").val();
-	  
-	  var currentUser = Meteor.user();
-	  currentId = currentUser._id;
-	  if (currentUser.hasOwnProperty('friends'))
-	      {
-		  var friends = currentUser.friends;
-		  friends.append(emailAddress);
-
-	      }
-	  else
-	      {
-		  var friends = [emailAddress];
-	      }
-
-	  Meteor.users.update({_id:currentId},{$set:{"friends":friends}});
-
-      }
-      
-	  */
       }});
 
 	  
@@ -197,21 +151,23 @@ twilio = Twilio("AC504dcad90a9b4647b890e9b1f71025ba", "c2ba241dca61e7c363703f9c3
 if (Meteor.isServer) {
   Meteor.startup(function () {
 
-	  /*	  twilio = Twilio("AC504dcad90a9b4647b890e9b1f71025ba", "c2ba241dca61e7c363703f9c393f8099");
-	  twilio.sendSms({
-		  to:'+16175711369', // Any number Twilio can deliver to
-		      from: '+14014415503', // Twilio number
-		      body: 'word to your mother.' // body of the SMS message
-		      }, function(err, responseData) { //this function is executed when a response is received from Twilio
-		  if (!err) { // "err" is an error received during the request, if any
-		      // "responseData" is a JavaScript object containing data received from Twilio.
-		      // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-		      // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-		      console.log(responseData.from); // outputs "+14506667788"
-		      console.log(responseData.body); // outputs "word to your mother."
+
+	  Meteor.methods({
+		  'postTwilio': function(){
+		      
+		      HTTP.call('post',
+				'https://api.twilio.com/2010-04-01/Accounts/AC504dcad90a9b4647b890e9b1f71025ba/SMS/Messages.json',
+				{
+				    params:{From:'+14014415503', To:'+14014394754', Body: 'ZOMG'},
+					auth: 'AC504dcad90a9b4647b890e9b1f71025ba:c2ba241dca61e7c363703f9c393f8099',
+					headers: {'content-type':'application/x-www-form-urlencoded'}
+				}, function () {
+				    console.log(arguments)
+					}
+				);
 		  }
 	      });
-	  */
+
 	  
 	  //console.log(client);
 	  
